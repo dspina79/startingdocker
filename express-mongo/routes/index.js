100e0b7be1a689caace18b843a7086c74a563314
Dave Spina <dspina79@gmail.com>
2021-12-13 21:17:57 -0500

    Iniital test page structure for to-dos

--- express-mongo/routes/index.js
+++ express-mongo/routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let subtitle = "This is a listing of personal to-dos."
+  res.render('index', { title: 'My Todo List', todos: [], subtitle: subtitle });
 });
 
 module.exports = router;
